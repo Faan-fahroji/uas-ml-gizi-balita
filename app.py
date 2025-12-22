9ba6c31fdae5d0c0bf2cecde7a833f5fc8381af9
Faan-fahroji <achmadfarhanfahroji@gmail.com>
2025-12-22 20:31:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -87,7 +87,8 @@ st.sidebar.info(f"Dataset aktif: **{DATA_FILE}**")
 # BERANDA
 # =============================
 if menu == "🏠 Beranda":
-    st.title("🧒📊 Dashboard Analisis Status Gizi Balita 2023")
+    st.title("Analisis Clustering Status Gizi Balita Berdasarkan Berat Badan Sangat
+Kurang dan Kurang Menggunakan Algoritma K-Means di Kabupaten Purwakarta dan Karawang, 2023")
     st.write(
         "Dashboard ini digunakan untuk menganalisis wilayah karawang status gizi balita "
         "berdasarkan indikator **Sangat Kurang (SK)** dan **Kurang (K)**."
@@ -215,3 +216,4 @@ elif menu == "🎯 Analisis Clustering":
 # =============================
 st.sidebar.markdown("---")
 st.sidebar.caption("© 2025 UAS Machine Learning – Status Gizi Balita")
+
